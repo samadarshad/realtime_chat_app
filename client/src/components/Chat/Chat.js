5c15611c1a890f50b249322480e49613f6ce17da
samad <samadarshad@hotmail.co.uk>
2021-04-22 19:17:21 +0100

    emit

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -8,6 +8,8 @@ const ENDPOINT = `localhost:5000`
 const Chat = ({ location }) => {
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search)
@@ -27,8 +29,34 @@ const Chat = ({ location }) => {
         }
     }, [location.search])
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message])
+        })
+    }, [messages])
+
+    const sendMessage = (event) => {
+        event.preventDefault()
+
+        if (message) {
+            socket.emit('sendMessage', message, () => {
+                setMessage('')
+            })
+        }
+    }
+
+    console.log(message, messages);
+
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <input
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                />
+            </div>
+        </div>
     )
 }
 
