e7fa19e17dca7378b46e4b39c940f0d0df795d98
samad <samadarshad@hotmail.co.uk>
2021-04-23 11:16:41 +0100

    wip cleaning

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,48 +1,23 @@
 import React, { useState, useEffect } from 'react'
-// import queryString from 'query-string'
-import io from 'socket.io-client'
 import './Chat.css'
 import InfoBar from '../InfoBar/InfoBar'
 import Input from '../Input/Input'
 import Messages from '../Messages/Messages'
 import TextContainer from '../TextContainer/TextContainer'
 
-// let socket
-
-// const ENDPOINT = process.env.REACT_APP_ENDPOINT || `localhost:5000`
-
 const Chat = ({ socket, name, room, initialMessages, initialUsers }) => {
-    // const [name, setName] = useState('')
-    // const [room, setRoom] = useState('')
+
     const [message, setMessage] = useState('')
-    const [messages, setMessages] = useState(initialMessages)
-    const [users, setUsers] = useState(initialUsers)
-
-    // useEffect(() => {
-    //     // const { name, room } = queryString.parse(location.search)
-    //     setName(name)
-    //     setRoom(room)
-
-    //     // socket = io(ENDPOINT)
-
-    //     // socket.emit('join', { name, room }, (error) => {
-    //     //     if (error) {
-    //     //         alert(error)
-    //     //     } else {
-    //     //         setName(name)
-    //     //         setRoom(room)
-    //     //     }
-    //     // })
-
-    //     // return () => {
-    //     //     socket.emit('disconnect')
-
-    //     //     socket.off()
-    //     // }
-    // }, [location.search])
+    const [messages, setMessages] = useState([])
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
-        console.log({ socket, name, room });
+        setMessages(initialMessages)
+        setUsers(initialUsers)
+        return () => {
+            socket.emit('disconnect')
+            socket.off()
+        }
     }, [])
 
     useEffect(() => {
